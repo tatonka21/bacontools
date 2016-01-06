bb8a404e49eac21712c22f3146215299f1f2abd6
Ilya Terentyev <bacondropped@gmail.com>
2016-01-07 00:30:24 +0300

    add __main__ guard for termdraw/csv.py
    Signed-off-by: Ilya Terentyev <bacondropped@gmail.com>

--- termdraw/csv.py
+++ termdraw/csv.py
@@ -9,6 +9,13 @@ import io
 import csv
 
 
+if __name__ == '__main__':
+	prefix = os.path.basename(__file__)
+	sys.stderr.write(prefix+': this module is not meant to be run'
+		' directly! exiting\n')
+	exit(1)
+
+
 def get_csv_data(filename):
 	with open(filename, 'r') as csvfile:
 		dialect = csv.Sniffer().sniff(csvfile.read(1024))
